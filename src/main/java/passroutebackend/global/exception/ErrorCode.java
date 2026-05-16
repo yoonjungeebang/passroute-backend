f002830c2d5b488df8ffcda0bc52e5d989cc3aa9
julianyeong <111331805+julianyeong@users.noreply.github.com>
2026-05-16 16:15:39 +0900

    fix: ErrorCode 수정

--- src/main/java/passroutebackend/global/exception/ErrorCode.java
+++ src/main/java/passroutebackend/global/exception/ErrorCode.java
@@ -26,7 +26,6 @@ public enum ErrorCode {
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재하지 않는 유저입니다."),
   USER_DELETED(HttpStatus.NOT_FOUND, "U002", "탈퇴 처리된 유저입니다."),
   DUPLICATE_EMAIL(HttpStatus.CONFLICT, "U003", "이미 사용중인 이메일입니다."),
-  USER_DELETED(HttpStatus.NOT_FOUND, "U002", "탈퇴 처리된 유저입니다."),
 
   // Interview
   SESSION_NOT_FOUND(HttpStatus.NOT_FOUND, "I001", "면접 세션을 찾을 수 없습니다."),
