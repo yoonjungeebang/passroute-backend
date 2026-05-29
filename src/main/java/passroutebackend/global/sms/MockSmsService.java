9ba8ffbebe50a98652e926c48929711c3317aeda
yezanee <rbfrk02@naver.com>
2026-05-30 00:38:05 +0900

    feat: CoolSMS 연동을 통한 실제 SMS 인증번호 발송 구현 (#68)
    - CoolSMS SDK(net.nurigo:sdk:4.3.0) 의존성 추가
    - CoolSmsService 구현 (dev/prod 프로필)
    - MockSmsService를 local/test 프로필로 제한
    - application-dev.yml, application-prod.yml에 SMS 설정 추가
    - cd-dev.yml, cd-prod.yml에 CoolSMS 환경변수 전달 추가

--- src/main/java/passroutebackend/global/sms/MockSmsService.java
+++ src/main/java/passroutebackend/global/sms/MockSmsService.java
@@ -1,10 +1,12 @@
 package passroutebackend.global.sms;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile({"local", "test"})
 public class MockSmsService implements SmsService {
 
     @Override
